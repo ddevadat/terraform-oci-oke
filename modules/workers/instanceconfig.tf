471750e9dd313723adaaadc3f637e690cef0511f
Deepak Devadathan <deepak.devadathan@oracle.com>
2024-12-10 16:14:04 +1100

    disabled create bv
    Signed-off-by: Deepak Devadathan <deepak.devadathan@oracle.com>

--- modules/workers/instanceconfig.tf
+++ modules/workers/instanceconfig.tf
@@ -123,13 +123,13 @@ resource "oci_core_instance_configuration" "workers" {
         is_pv_encryption_in_transit_enabled = each.value.pv_transit_encryption
       }
 
-      create_details {
-        // Limit to first candidate placement AD for cluster-network; undefined for all otherwise
-        availability_domain = each.value.mode == "cluster-network" ? element(each.value.availability_domains, 1) : null
-        compartment_id      = each.value.compartment_id
-        display_name        = each.key
-        kms_key_id          = each.value.volume_kms_key_id
-      }
+      # create_details {
+      #   // Limit to first candidate placement AD for cluster-network; undefined for all otherwise
+      #   availability_domain = each.value.mode == "cluster-network" ? element(each.value.availability_domains, 1) : null
+      #   compartment_id      = each.value.compartment_id
+      #   display_name        = each.key
+      #   kms_key_id          = each.value.volume_kms_key_id
+      # }
     }
 
     dynamic "secondary_vnics" {
