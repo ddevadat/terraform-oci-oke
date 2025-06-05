1326e648bc8f6ac98b35bbac5ab00b99c5bb2a43
Deepak Devadathan <deepak.devadathan@oracle.com>
2025-06-05 12:44:58 +1000

    fix: adding the full kubelet args
    to avoid the case where there is not taints
    
    Signed-off-by: Deepak Devadathan <deepak.devadathan@oracle.com>

--- modules/workers/cloudinit-oke.sh
+++ modules/workers/cloudinit-oke.sh
@@ -25,7 +25,7 @@ function run_oke_init() { # Initialize OKE worker node
   else # Retrieve base64-encoded script content from http, e.g. instance metadata
     local oke_init_url='http://169.254.169.254/opc/v2/instance/metadata/oke_init_script'
     curl --fail -H "Authorization: Bearer Oracle" -L0 "$${oke_init_url}" \
-      | base64 --decode >/var/run/oke-init.sh && bash /var/run/oke-init.sh --kubelet-extra-args ${kubelet_extra_args}
+      | base64 --decode >/var/run/oke-init.sh && bash /var/run/oke-init.sh ${kubelet_extra_args}
   fi
 }
 
