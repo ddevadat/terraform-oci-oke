24e718315a507ec4c21a4d865c607616807fa868
Deepak Devadathan <deepak.devadathan@oracle.com>
2025-06-24 11:59:18 +1000

    fix: added a validation
    Signed-off-by: Deepak Devadathan <deepak.devadathan@oracle.com>

--- modules/extensions/custom_apps.tf
+++ modules/extensions/custom_apps.tf
@@ -1,7 +1,25 @@
 # Copyright (c) 2023 Oracle Corporation and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl
 
+locals {
+  has_inline_helmfile     = var.custom_app_helmfile_values != ""
+  has_file_helmfiles      = length(var.custom_app_helmfile_values_files) > 0
+  invalid_helmfile_config = var.custom_app_install && !local.has_inline_helmfile && !local.has_file_helmfiles
+  should_run_helmfile     = var.custom_app_install && (local.has_inline_helmfile || local.has_file_helmfiles)
+}
+
+resource "null_resource" "validate_custom_app_inputs" {
+  count = local.invalid_helmfile_config ? 1 : 0
+
+  provisioner "local-exec" {
+    command = "echo 'ERROR: custom_app_helmfile_values or custom_app_helmfile_values_files must be provided when custom_app_install = true' && exit 1"
+  }
+}
+
 resource "null_resource" "create_helmfile_dir" {
+
+  count = local.should_run_helmfile ? 1 : 0
+
   connection {
     bastion_host        = var.bastion_host
     bastion_user        = var.bastion_user
@@ -19,7 +37,7 @@ resource "null_resource" "create_helmfile_dir" {
 }
 
 resource "null_resource" "upload_inline_helmfile" {
-  count      = var.custom_app_helmfile_values != "" ? 1 : 0
+  count      = local.should_run_helmfile && local.has_inline_helmfile ? 1 : 0
   depends_on = [null_resource.create_helmfile_dir]
 
   connection {
@@ -33,37 +51,39 @@ resource "null_resource" "upload_inline_helmfile" {
 
   provisioner "file" {
     content     = var.custom_app_helmfile_values
-    destination = "${local.yaml_manifest_path}/helmfile/cilium-helmfile.yaml"
+    destination = "${local.yaml_manifest_path}/helmfile/custom-app-helmfile.yaml"
   }
 }
 
-resource "null_resource" "upload_helmfile_files" {
-  for_each   = { for file in var.custom_app_helmfile_values_files : file => file }
-  depends_on = [null_resource.create_helmfile_dir]
+# resource "null_resource" "upload_helmfile_files" {
+#   for_each   = { for file in var.custom_app_helmfile_values_files : file => file }
+#   depends_on = [null_resource.create_helmfile_dir]
 
-  connection {
-    bastion_host        = var.bastion_host
-    bastion_user        = var.bastion_user
-    bastion_private_key = var.ssh_private_key
-    host                = var.operator_host
-    user                = var.operator_user
-    private_key         = var.ssh_private_key
-  }
+#   connection {
+#     bastion_host        = var.bastion_host
+#     bastion_user        = var.bastion_user
+#     bastion_private_key = var.ssh_private_key
+#     host                = var.operator_host
+#     user                = var.operator_user
+#     private_key         = var.ssh_private_key
+#   }
 
-  provisioner "file" {
-    source      = each.value
-    destination = "${local.yaml_manifest_path}/helmfile/${basename(each.value)}"
-  }
+#   provisioner "file" {
+#     source      = each.value
+#     destination = "${local.yaml_manifest_path}/helmfile/${basename(each.value)}"
+#   }
 
-  triggers = {
-    file_path = each.value
-  }
-}
+#   triggers = {
+#     file_path = each.value
+#   }
+# }
+
 
 resource "null_resource" "helmfile_apply" {
+  count = local.should_run_helmfile ? 1 : 0
+
   depends_on = [
     null_resource.upload_inline_helmfile,
-    null_resource.upload_helmfile_files
   ]
 
   provisioner "remote-exec" {
